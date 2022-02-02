b5e50d173d37db97e03dff42746106ad41e7aff7
alphadzingina <alphaadx6@gmail.com>
2022-02-02 04:24:07 +0100

    get andomly celebrity

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from art import logo
+from game_data import data
+import random
+
 
 print(logo)
 
+#rabdomly select celebrity from data
+celebrity1 = random.choice(data)
+celebrity2 = random.choice(data)
+
+print(celebrity1)
+print(celebrity2)
