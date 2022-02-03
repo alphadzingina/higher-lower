2d70b41046eb0b7479bf00d7389d57505285e5a1
alphadzingina <alphaadx6@gmail.com>
2022-02-03 02:28:25 +0100

    change variable 'player' to 'account', print data

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ import random
 
 print(logo)
 
-player_a = random.choice(data)
-player_b = random.choice(data)
-if player_a == player_b:
-    player_b = random.choice(data)
+account_a = random.choice(data)
+account_b = random.choice(data)
+if account_a == account_b:
+    account_b = random.choice(data)
+
+def format_data(account):
+    """"format the account data in printable format"""
+    account_name = account["name"]
+    account_descr = account["description"]
+    account_country = account["country"]
+    return f"{account_name}, a {account_descr} from {account_country}"
+
+print(f"Compare A: {format_data(account_a)}")
+print(f"Compare B: {format_data(account_b)}")
