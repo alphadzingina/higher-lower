b4838569265c854292c868f1b917c5d0c73d0c65
alphadzingina <alphaadx6@gmail.com>
2022-02-03 02:59:03 +0100

    check if user is correct

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if account_a == account_b:
     account_b = random.choice(data)
 
 def format_data(account):
-    """"format the account data in printable format"""
+    """"Takes the account data and returns in printable format"""
     account_name = account["name"]
     account_descr = account["description"]
     account_country = account["country"]
@@ -21,3 +21,14 @@ print(vs)
 print(f"Against B: {format_data(account_b)}")
 
 guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+def check_answer(guess, a_followers, b_followers):
+    """take the user guess and follower counts and returns if they got it right"""
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
+
+a_follower_count = account_a["follower_count"]
+b_follower_count = account_b["follower_count"]
+is_correct = check_answer(guess, a_follower_count, b_follower_count)
